25ec8de0c531d87d0a3a97c33157eb8602131003
Andres-Lozano <147666704+Andres-Lozano@users.noreply.github.com>
2024-07-10 16:49:54 -0500

    Add files via upload

---
+++ EjemplosSOLID/EjemplosSOLID/src/paquete10d/Digo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete10d;
+
+public class Digo implements APIMovie {
+
+    private String apiM;
+
+    @Override
+    public void establecerApiKey(String ak) {
+        apiM = ak + 22222;
+    }
+
+    @Override
+    public String obtenerApiKey() {
+        return apiM;
+    }
+
+}
+
+
